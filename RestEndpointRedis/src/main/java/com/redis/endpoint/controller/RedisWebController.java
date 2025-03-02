db2c317969198d520b80cd680cabfe398566cdc9
DESKTOP-J7F8RBI\Usuario <herberttb@gmail.com>
2025-03-02 03:47:46 -0300

    adjust response

--- RestEndpointRedis/src/main/java/com/redis/endpoint/controller/RedisWebController.java
+++ RestEndpointRedis/src/main/java/com/redis/endpoint/controller/RedisWebController.java
@@ -1,12 +1,14 @@
 package com.redis.endpoint.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 import com.redis.endpoint.model.Entity;
 import com.redis.endpoint.service.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -15,43 +17,43 @@ import org.springframework.web.bind.annotation.*;
 public class RedisWebController {
 
     private static final Logger LOG = LoggerFactory.getLogger(RedisWebController.class);
-
+    private static final Gson gson = new Gson();
     @Autowired
     RedisService redisSender;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/data/create")
-    public String newEntity(@RequestBody Entity entity) throws JsonProcessingException {
+    public ResponseEntity<String> newEntity(@RequestBody Entity entity) throws JsonProcessingException {
         redisSender.sendData(entity);
-        return "Message sent to the Kafka JavaInUse Successfully";
+        return ResponseEntity.ok(gson.toJson("Message sent to the Kafka JavaInUse Successfully"));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/event/create")
-    public String newEvent(@RequestBody Entity entity) throws JsonProcessingException {
+    public ResponseEntity<String> newEvent(@RequestBody Entity entity) throws JsonProcessingException {
         redisSender.sendRule(entity);
-        return "Message sent to the Kafka JavaInUse Successfully";
+        return ResponseEntity.ok(gson.toJson("Message sent to the Kafka JavaInUse Successfully"));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/rule/create")
-    public String createRule(@RequestBody Entity entity) throws JsonProcessingException {
+    public ResponseEntity<String> createRule(@RequestBody Entity entity) throws JsonProcessingException {
         redisSender.sendRule(entity);
-        return "Message sent to the Kafka JavaInUse Successfully";
+        return ResponseEntity.ok(gson.toJson("Message sent to the Kafka JavaInUse Successfully"));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "/rule/update")
-    public String updateRule(@RequestBody Entity entity) throws JsonProcessingException {
+    public ResponseEntity<String> updateRule(@RequestBody Entity entity) throws JsonProcessingException {
         redisSender.sendRule(entity);
-        return "Message sent to the Kafka JavaInUse Successfully";
+        return ResponseEntity.ok(gson.toJson("Message sent to the Kafka JavaInUse Successfully"));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping(value = "/rule/delete")
-    public String deleteRule(@RequestBody Entity entity) throws JsonProcessingException {
+    public ResponseEntity<String> deleteRule(@RequestBody Entity entity) throws JsonProcessingException {
         redisSender.sendRule(entity);
-        return "Message sent to the Kafka JavaInUse Successfully";
+        return ResponseEntity.ok(gson.toJson("Message sent to the Kafka JavaInUse Successfully"));
     }
 
 }
